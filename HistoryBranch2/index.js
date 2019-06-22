4dc3dc4adf0965282acd3547d0546a4327008242
Slayzur02 <ducanh.tranpro@gmail.com>
2019-06-23 02:11:49 +0700

    use this

---
+++ HistoryBranch2/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll',()=>{
+    var wScroll = window.scrollY;
+    console.log(wScroll);
+    const para = document.getElementsByClassName("starimag")[0];
+    para.style.transform = 'translate(0px, '+ wScroll/(window.innerHeight) +'%)';
+    if (wScroll>500){
+        document.body.style.backgroundColor = '#000021'
+    }
+    else {
+        document.body.style.backgroundColor = '#0E0E15' 
+    }
+});
